779edc20d9fc9bade02eeaccacef4540b789df5f
Ali Masoud <alimasoud827@gmail.com>
2025-05-13 13:46:32 +0300

    Project: Initial Prontend complete

---
+++ MySQL_Project/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    loadHTMLTable([]);
+});
+
+function loadHTMLTable(data){
+    const table = document.querySelector("table tbody");
+    // let tableHTML = "";
+    if(data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No data available</td></tr>";
+    }
+        
+}
